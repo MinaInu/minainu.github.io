0e3531e6a13e580d9cdcfdf921355edbc2e24e51
MinaInu <minainutoken@gmail.com>
2024-06-04 13:03:37 +0200

    Update

---
+++ script.js
@@ -0,0 +1,18 @@
+var addresses;
+
+fetch('addresses.json')
+    .then((response) => response.json())
+    .then((json) => addresses = json);
+
+function onChange() {
+    // console.log(addresses);
+    var input = document.getElementById("address").value;
+    // console.log(input);
+    if (input.length == 55) {
+        if (addresses.includes(input.slice(-8))) {
+            document.getElementById("eligible").textContent = "Your account is eligible for the airdrop";
+        } else {
+            document.getElementById("eligible").textContent = "Your account is not eligible for the airdrop";
+        }
+    }
+}
